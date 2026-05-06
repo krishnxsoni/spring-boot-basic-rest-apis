e00e0475b5c0bb7f6756f847d8966e98ed429498
Krishna <krishna010502@gmail.com>
2026-05-07 01:27:55 +0530

    Added Spring Thymeleaf - init

---
+++ src/main/java/com/example/test/controller/AboutController.java
@@ -0,0 +1,14 @@
+package com.example.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AboutController
+{
+    @RequestMapping(value = "/about",method = RequestMethod.GET)
+    public String returnAboutPage(){
+        return "about"; //about.html
+    }
+}
