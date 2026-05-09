ec7fbf2f2fd15a1346361bf9e402475d7006090c
Krishna <krishna010502@gmail.com>
2026-05-09 21:32:08 +0530

    Added Spring Thymeleaf - init2

---
+++ src/main/java/com/example/test/utilities/Constants.java
@@ -0,0 +1,11 @@
+package com.example.test.utilities;
+
+/**
+*This Constant Class is created to use the default variable values from here only!
+*
+*/
+
+public class Constants
+{
+    public static final String EMPTY_STRING = "";
+}
