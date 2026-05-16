36a211d54db35a6596510942782afc5514d7864e
Krishna <krishna010502@gmail.com>
2026-05-16 23:05:22 +0530

    Added basic form validations

--- src/main/java/com/example/test/controller/UserRegisterController.java
+++ src/main/java/com/example/test/controller/UserRegisterController.java
@@ -1,10 +1,10 @@
 package com.example.test.controller;
 
 import com.example.test.entities.User;
-import com.example.test.utilities.Constants;
+import com.example.test.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.Banner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserRegisterController
 {
-
+    @Autowired
+    private UserService userService;
     private static final Logger log = LoggerFactory.getLogger(UserRegisterController.class);
 
     @GetMapping(value = "/new-user")
@@ -25,12 +26,19 @@ public class UserRegisterController
 
 
     @PostMapping(value = "/submit-user-details")
-    public String createNewUser(@ModelAttribute User user, Model model)
+    public String checkUserLogin(@ModelAttribute User user, Model model)
     {
         log.info(":: Inside create-new-user Page :: ");
         String username  = user.getUserName();
         log.info(":: username :: "+username);
-        model.addAttribute("username", username);
-        return "submit-user-details";
+
+        User user1  = userService.getUserByUsername(username);
+        if(user1==null){
+            return "user-not-found";
+        }
+        else{
+            model.addAttribute("username", user1.getFirstName()+" "+user1.getLastName());
+            return "submit-user-details";
+        }
     }
 }
